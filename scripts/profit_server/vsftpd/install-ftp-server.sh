4531df5a2d1a4a5530aa3f1157727cb2bb576b85
train-to-cupertino <49920253+train-to-cupertino@users.noreply.github.com>
2022-11-09 17:15:14 +0300

    Update install-ftp-server.sh

--- scripts/profit_server/vsftpd/install-ftp-server.sh
+++ scripts/profit_server/vsftpd/install-ftp-server.sh
@@ -1,2 +1,3 @@
 # Change password and "ftpdata" folder
 docker rm ftp && docker run --detach --env FTP_PASS=SOMEPASS --env FTP_USER=user1 --name ftp --publish 20-21:20-21/tcp --publish 40000-40009:40000-40009/tcp --volume /ftpdata:/home/user1 garethflowers/ftp-server
+# Use --restart always to prevent ftp server downtime
