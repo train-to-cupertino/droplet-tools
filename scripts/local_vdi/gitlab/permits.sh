945c6d68be0bc4290c408e5df0698c4d8f8cea82
train-to-cupertino <49920253+train-to-cupertino@users.noreply.github.com>
2023-01-17 18:23:41 +0300

    Update permits.sh

--- scripts/local_vdi/gitlab/permits.sh
+++ scripts/local_vdi/gitlab/permits.sh
@@ -1,6 +1,5 @@
-#docker exec -it gitlab_app update-permissions
-sudo chmod -R 777 ~/gitlab/export/
-sudo chmod 600 ~/gitlab/export/containers/gitlab-config/ssh_host_ecdsa_key
-sudo chmod 600 ~/gitlab/export/containers/gitlab-config/ssh_host_ed25519_key
-sudo chmod 600 ~/gitlab/export/containers/gitlab-config/ssh_host_rsa_key
-docker compose exec -it gitlab_app update-permissions
+sudo chmod -R 777 ./export/
+sudo chmod 600 ./export/containers/gitlab-config/ssh_host_ecdsa_key
+sudo chmod 600 ./export/containers/gitlab-config/ssh_host_ed25519_key
+sudo chmod 600 ./export/containers/gitlab-config/ssh_host_rsa_key
+sudo docker compose exec -it gitlab_app update-permissions
